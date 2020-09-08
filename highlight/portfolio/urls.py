60b7969884deb9d309ddf0ea48588134833b13bd
Kyoungwook <kippsr3@naver.com>
2020-09-08 17:42:32 +0900

    edit, delete 추가, list page 생성

--- highlight/portfolio/urls.py
+++ highlight/portfolio/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('list/', views.list, name = 'list'),
     path('<int:portfolio_id>/', views.detail, name = 'detail'),
     path('new/', views.portfolio_new, name = 'new'),
+    path('<int:portfolio_id>/edit/', views.portfolio_edit, name = 'edit'),
+    path('<int:portfolio_id>/delete/', views.portfolio_delete, name = 'delete'),
 ]
--- highlight/portfolio/views.py
+++ highlight/portfolio/views.py
@@ -4,9 +4,13 @@ from django.utils import timezone
 from .forms import PortfolioForm
 
 # Create your views here.
+def list(request):
+    portfolio = Portfolio.objects
+    return render(request, 'portfolio/portfolio_list.html', {'portfolio':portfolio})
+
 def detail(request, portfolio_id):
     portfolio_detail = get_object_or_404(Portfolio, pk = portfolio_id)
-    return render(request, 'portfolio/detail.html', {'portfolio' : portfolio_detail})
+    return render(request, 'portfolio/portfolio_detail.html', {'portfolio' : portfolio_detail})
 
 def portfolio_new(request):
     if request.method == "POST":
@@ -14,8 +18,26 @@ def portfolio_new(request):
         if form.is_valid():
             portfolio = form.save(commit=False)
             # request.user.is_authenticated
+            portfolio.editor = request.user
             portfolio.save()
             return redirect('detail', portfolio_id = portfolio.pk)
     else:
         form = PortfolioForm()
     return render(request, 'portfolio/portfolio_new.html', {'form': form})
+
+def portfolio_edit(request, portfolio_id):
+    portfolio = get_object_or_404(Portfolio, pk = portfolio_id)
+    if request.method == "POST":
+        form = PortfolioForm(request.POST, instance = portfolio)
+        if form.is_valid():
+            portfolio = form.save(commit=False)
+            portfolio.save()
+            return redirect('detail', portfolio_id = portfolio.pk)
+    else:
+        form = PortfolioForm(instance = portfolio)
+    return render(request, 'portfolio/portfolio_edit.html', {'form':form})
+
+def portfolio_delete(request, portfolio_id):
+    portfolio = get_object_or_404(Portfolio, pk=portfolio_id)
+    portfolio.delete()
+    return redirect('list')
