184de7a9d76390bc527da57b74cd738f74b03082
BIINNNN <seobin99@gmail.com>
2020-09-18 16:10:15 +0900

    네비바 이미지 홈링크 연결

--- highlight/user/form.py
+++ highlight/user/form.py
@@ -1,3 +1,7 @@
 from django import forms
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = {'username', 'email', 'password','user_type', 'name', 'phone_number' }
