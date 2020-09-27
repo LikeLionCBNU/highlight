9c77e5d1de6437d6b11bfb24229fc0c8b0a73b97
choisaywhy <sehwa98@icloud.com>
2020-09-27 11:57:38 +0900

    0926수정사항

--- highlight/user/views.py
+++ highlight/user/views.py
@@ -8,15 +8,17 @@ def signup(request):
     if request.method == "POST":
         if request.POST["password"] == request.POST["password_confirm"]:
             form = UserSignupForm(request.POST)
+            print(form.errors)
+
             if form.is_valid():
                 user = User()
                 user = form.save(commit=False)
+                user.save()
                 auth.login(request,user)
                 return redirect('main')
             else:
                 form = UserSignupForm()
                 print('form not valid')
-                print(form.errors)
                 return render(request, 'user/signup.html',{'form':form})
         else:
             form = UserSignupForm()
@@ -28,24 +30,6 @@ def signup(request):
         return render(request, 'user/signup.html',{'form':form})
 
 
-# def signup(request):
-#     form = UserSignupForm()
-#     if request.method == "POST":
-#         if form.is_valid():
-#             if request.POST["password"] == request.POST["password_confirm"]:
-#                 user = form.save(commit=False)
-#                 auth.login(request,user)
-#                 return redirect('user:login')
-#             else:
-#                 return render(request, 'user/signup.html')
-#         else:
-#             form = UserSignupForm()
-#         return render(request, 'user/signup.html',{'form':form})
-#     return render(request, 'user/signup.html',{'form':form})
-
-    
-
-
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -53,7 +37,7 @@ def login(request):
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             print("인증성공")
-            auth.login(request, user)
+            auth.login(request.user)
             return redirect('main')
         else:
             print("인증실패")
@@ -62,8 +46,14 @@ def login(request):
         return render(request, 'user/login.html')
 
 def logout(request):
-    auth.logout(request)
+    auth.logout(request.user)
     return redirect('user:login')
 
 def mypage(request):
     return render(request, 'home')
+
+
+    # 경욱님
+    # 포트폴리오 평점 적용
+    # 리뷰 필터링
+    # 검색하기 기능 
